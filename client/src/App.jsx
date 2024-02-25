3c6426072d0a491c12b831c45584fda07a99c4f8
mesfin beyene <mesfinbaby@gmail.com>
2024-02-25 14:13:33 -0800

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
 import react from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Dashboared from "./pages/Dashboared.jsx";
+import Projects from "./pages/Projects.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import SignIn from "./pages/SignIn.jsx";
 
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboared" element={<Dashboared />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
---
+++ client/src/pages/Dashboared.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboared = () => {
+  return <div>Dashboared</div>;
+};
+
+export default Dashboared;
